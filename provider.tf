21bfb78d59a7e4b033c0041bf1e90ac42662c161
nonso-chiagunye <nonsoasoby@hotmail.com>
2023-06-24 19:53:04 +0400

    Added Backend Variables

--- provider.tf
+++ provider.tf
@@ -4,16 +4,16 @@ provider "aws" {
   region = var.AWS_REGION
 }
 
-# This is necessary if you use remote backend
+# This is necessary if you use remote backend. Comment it out if you use local backend. 
 terraform {
   required_version = "~> 1.0"
 
   backend "s3" {
-    bucket         = "bucket_name"
-    key            = "state_location"
-    region         = "aws_region"
-    dynamodb_table = "state_lock"
-    encrypt        = "true"
+    bucket         = "${var.backend_bucket_name}"
+    key            = "${var.backend_state_location}"
+    region         = "${var.backend_region}"
+    dynamodb_table = "${var.dynamodb_id}"
+    encrypt        =  var.encrypt_tfstate 
   }
 }
 
